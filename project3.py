e500565c7a97e356301a3746c316174251e0bc20
Kenneth_-_Zheng <145678529+KENNETHaZHENG@users.noreply.github.com>
2024-05-21 13:54:53 -0400

    Update project3.py

--- project3.py
+++ project3.py
@@ -13,7 +13,7 @@ for quarters in range(num_q + 1):
 
 print(*coord_)
 
-
+# I have removed my classmates' work from this version
 table = ["\n", 0, 1, 1, 0, "\n", 1, 0, 1, 1, "\n", 1, 1, 0, 1, "\n", 1, 1, 1, 1, "\n", 0, 1, 1, 0]
 print(*table)
 
@@ -21,80 +21,105 @@ print(*table)
 coins = (num_q, num_d)
 
 
-def game(p: tuple, e=0):  # step 2: choose a turn
-    # figure out how to restart
+def game(p: tuple, e):
     quarter, dime = p  # split the tuple
-    # index = coord_.index((quarter, dime))
-    # state = table[index]
-    counter = 0
+
+    # says the total number od quarters and dimes left:
     print(f"------\nquarters: {quarter}, dimes: {dime}")
-    if dime >= 0 and quarter >= 0:  # state; not (0,0)
-        turn = e  # represent whose turn it is
-        if turn == 0:
-            i = input("dimes or quarters: ")  # choice
-            if "dime" in i or "d" in i:
-                d = input("How many do you want to take?:  ")
-                if "1" in d and dime - 1 >= 0:  # taking the dimes
-                    dime -= 1
-                elif "2" in d and dime - 2 >= 0:
-                    dime -= 2
-                else:  # restart function
-                    print("this not allowed, go again \n")
-                    return game((quarter, dime), turn)
-            elif "quarter" in i or "q" in i:  # taking the quarters
-                q = input("How many do you want to take?:  ")
-                if "1" in q and quarter - 1 >= 0:  # taking the dimes
-                    quarter -= 1
-                elif "2" in q and quarter - 2 >= 0:
-                    quarter -= 2
-                elif "3" in q and quarter - 3 >= 0:
-                    quarter -= 3
-                else:  # restart function
-                    print("this not allowed, go again \n")
-                    return game((quarter, dime), turn)
-            else:  # restart
-                print("this not allowed, go again \n")
-                return game((quarter, dime), turn)
-            if (quarter, dime) == (0, 0):  # you take the last one, you win
-                print((quarter, dime))
-                return "You Win"
-            turn = 1
-        print((quarter, dime))
-        if turn == 1:
-            print("AI turn: ")
-            choice = 3  # how much to take
-            action = 0
-            while choice > 0:  # (1,1)
-                if (quarter - choice) >= 0 or (dime - choice) >= 0:  # 3 -> x, 2 -> x, 1 -> v
-                    if quarter - choice >= 0:  # 1 -> v
-                        wtv = coord_.index((quarter - choice, dime))  # quarters (0,1)
-                        if table[wtv] == 0:  # winning state on taking quarter (0,1) = 1
+    # ----
+    if e == 0:  # player turn if e = 0
+        # ----
+        i = input("dimes or quarters: ")  # choosing which coin
+
+        if "dime" in i or "d" in i:  # take dimes
+            counter = 2  # can only take at max of 2
+        elif "quarter" in i or "q" in i:  # taking the quarters
+            counter = 3  # can take at max of 3
+        else:  # restart
+            print("Not a valid option, go again. \n")
+            return game((quarter, dime), e)
+        # ----
+        t = input("How many do you want to take?:  ")  # how much to take
+        # check if int(t) is good and it's less than the counter:
+        if t.isdigit() and int(t) <= counter:  # checks if t has digits
+            if counter == 2 and dime - int(t) >= 0:  # if dime
+                dime -= int(t)
+            elif counter == 3 and quarter - int(t) >= 0:  # if quarter
+                quarter -= int(t)
+            else:
+                print("No\n")
+                return game((quarter, dime), e)
+        else:  # restart
+            print("Not allowed, go again \n")
+            return game((quarter, dime), e)
+        # ----
+        if (quarter, dime) == (0, 0):  # you take the last one, you win
+            print((quarter, dime))
+            return "You Win"
+        print((quarter, dime))  # shows the aftermath
+        e = 1  # switch turn
+    # ----
+    if e == 1:  # AI turn when e = 1
+        print("\nBoB the Destroyer of the Abyss and holder of the Morning Star's turn: ")
+
+        choice = 3  # how much to take
+        action = 0  # know whether AI took something
+        while choice > 0:
+
+            # check if taking won't go into negatives:
+            if (quarter - choice) >= 0 or (dime - choice) >= 0:
+                if quarter - choice >= 0:  # 4 - 2
+                    # ----
+                    # find the index of (quarter, dime) after taking quarter
+                    wtv = coord_.index((quarter - choice, dime))
+                    if table[wtv] == 0:  # winning state on taking quarter
+                        quarter -= choice  # take the thing
+                        action = 1
+                        print(f"They took {choice} quarter(s)")
+                        break
+                # ----
+                if choice <= 2:  # dimes since we can't take 3 dimes
+                    # ----
+                    if dime - choice >= 0:
+                        nice = coord_.index((quarter, dime - choice))
+                        if table[nice] == 0:  # winning state on taking dimes
+                            dime -= choice
+                            action = 1
+                            print(f"They took {choice} dime(s)")
+                            break
+                choice -= 1  # if taking neither makes winning, take one less
+
+            elif quarter - choice < 0 or dime - choice < 0:
+                choice -= 1
+        # ----
+        if action == 0:  # if no winning state, take a random number of quarters or dimes
+            while action == 0:
+                c = random.randint(1, 2)  # random number generator (RNG)
+                # 1 = quarters
+                if quarter != 0 and c == 1:
+                    while quarter == quarter:
+                        choice = random.randint(1, 3)
+                        if quarter - choice >= 0:
                             quarter -= choice
+                            print(f"took {choice} quarter(s)")
                             action = 1
                             break
-                    if choice <= 2:  # dimes
+                # ----
+                elif dime != 0 and c == 2:
+                    while dime == dime:
+                        choice = random.randint(1, 2)
                         if dime - choice >= 0:
-                            nice = coord_.index((quarter, dime - choice))
-
-                            if table[nice] == 0:  # winning state on taking dimes
-                                dime -= choice
-                                action = 1
-                                break
-                    choice -= 1
-                # if taking neither makes winning, take one less
-
-                elif quarter - choice < 0 or dime - choice < 0:  # lose_state = 2
-                    choice -= 1
-            if action == 0:
-                if quarter - 1 >= 0:
-                    quarter -= 1
-                elif dime - 1 >= 0:
-                    dime -= 1
-            if (quarter, dime) == (0, 0):  # if AI takes the last one, you lose
-                return "You Lose"
-            turn = 0
-        print((quarter, dime))
-        return game((quarter, dime), turn)
+                            dime -= choice
+                            print(f"took {choice} dime(s)")
+                            action = 1
+                            break
+        # ----
+        if (quarter, dime) == (0, 0):  # if AI takes the last one, you lose
+            print((quarter, dime))
+            return "You Lose"
+        e = 0
+    print((quarter, dime))
+    return game((quarter, dime), e)  # recursive: Go again until (0,0)
 
 
 print(game(coins, 1))
